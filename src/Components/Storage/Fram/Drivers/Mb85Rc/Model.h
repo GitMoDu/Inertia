dfc121f6828566a83daeae0224acad5e167f60b3
GitModu <batatas@gmail.com>
2026-04-26 18:54:02 +0100

    Added MB85RC FRAM driver.

---
+++ src/Components/Storage/Fram/Drivers/Mb85Rc/Model.h
@@ -0,0 +1,24 @@
+#ifndef _INERTIA_COMPONENTS_STORAGE_MB85_RC_h
+#define _INERTIA_COMPONENTS_STORAGE_MB85_RC_h
+
+#include "../../Model.h"
+
+namespace Inertia
+{
+	namespace Components
+	{
+		namespace Storage
+		{
+			namespace Mb85Rc
+			{
+				static constexpr uint32_t LOG_TAG = 2816067516; // Random unique tag for MB85RC logs.
+
+				enum class LogCodeEnum : uint8_t
+				{
+					EnumCount
+				};
+			}
+		}
+	}
+}
+#endif
